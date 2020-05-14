498dafe8ddea90143634de7fa6a2b378eb74c532
Tom <thomas.spencer@codurance.com>
2020-05-14 20:02:48 +0100

    Add comments to the actions and reducers files

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,9 @@
+// Actions are Javascript objects
+// which have a type property and
+// any other arbitrary properties to
+// represent the action that is being
+// done
 
-// Action types
 export const ADD_TODO = 'ADD_TODO';
 export const TOGGLE_TODO = 'TOGGLE_TODO';
 export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
--- src/reducers.js
+++ src/reducers.js
@@ -1,3 +1,8 @@
+// reducers are pure functions that take
+// the previous state and an action and
+// return the new state that the action has
+// brought about
+
 import {VisibilityFilters} from "./actions";
 
 const initialState = {
