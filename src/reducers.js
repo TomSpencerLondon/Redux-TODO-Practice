417511d33e615ef8c2c75bdf4aa3599253bd059b
Tom <thomas.spencer@codurance.com>
2020-05-14 20:17:51 +0100

    Add switch for todoApp reducer

--- src/reducers.js
+++ src/reducers.js
@@ -3,7 +3,7 @@
 // return the new state that the action has
 // brought about
 
-import {VisibilityFilters} from "./actions";
+import {ADD_TODO, SET_VISIBILITY_FILTER, VisibilityFilters} from './actions';
 
 const initialState = {
   visibilityFilter: VisibilityFilters.SHOW_ALL,
@@ -11,5 +11,23 @@ const initialState = {
 }
 
 function todoApp(state = initialState, action) {
-  return state;
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return Object.assign({}, state, {
+        visibilityFilter: action.filter,
+      })
+    case ADD_TODO:
+      return Object.assign({}, state, {
+        todos: [
+          ...state.todos,
+          {
+            text: action.text,
+            completed: false
+          }
+        ],
+      })
+    default:
+      return state
+  }
 }
+
